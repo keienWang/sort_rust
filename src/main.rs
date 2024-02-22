fe471b02fd9cf4f84a7f33c72a0ab2947ce018bc
keienWang <42377006+keienWang@users.noreply.github.com>
2024-02-22 10:12:43 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Serialize, Deserialize};
+use serde_json::{Result, Value};
+
+
+struct Person {
+    name: String,
+    age: u8,
+    is_student: bool,
 }
 
+fn main() -> Result<()> {
+   
+   
+    Ok(())
+}
